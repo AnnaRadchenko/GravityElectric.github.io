086d5f7411eb103289ee41be8230674b9a3a2bf7
AnnaRadchenko <i_believe_i_can_fly_@ukr.net>
2016-04-14 12:35:41 +0300

    project control temperature

---
+++ js/custom.js
@@ -0,0 +1,162 @@
+$('.graph-1').click(function(){
+        $(".graph1").toggle();
+})
+
+$('.graph-2').click(function(){
+    $(".graph2").toggle();
+})
+
+$('.graph-3').click(function(){
+    $(".graph3").toggle();
+})
+
+var snd = new Audio('http://www.freesfx.co.uk/rx2/mp3s/2/2710_1329133090.mp3');
+
+document.addEventListener('change', function(e){
+if(e.target.parentNode.className.indexOf('checkboxControl') != -1){
+    snd.currentTime = 0;
+    snd.play(); 
+}
+});
+
+(function ($) {
+    "use strict";
+    var mainApp = {
+
+        main_fun: function () {
+            $('#main-menu').metisMenu();
+            $(window).bind("load resize", function () {
+                if ($(this).width() < 768) {
+                    $('div.sidebar-collapse').addClass('collapse')
+                } else {
+                    $('div.sidebar-collapse').removeClass('collapse')
+                }
+            });         
+        },
+
+        initialization: function () {
+            mainApp.main_fun();
+        }
+    }
+
+
+    $(document).ready(function () {
+        mainApp.main_fun();
+    });
+
+}(jQuery));
+
+(function($) {
+
+  $.fn.menumaker = function(options) {
+      
+      var cssmenu = $(this), settings = $.extend({
+        title: "ВЫБРАТЬ КОМНАТУ",
+        format: "dropdown",
+        breakpoint: 768,
+        sticky: false
+      }, options);
+
+      return this.each(function() {
+        cssmenu.find('li ul').parent().addClass('has-sub');
+        if (settings.format != 'select') {
+          cssmenu.prepend('<div id="menu-button">' + settings.title + '</div>');
+          $(this).find("#menu-button").on('click', function(){
+            $(this).toggleClass('menu-opened');
+            var mainmenu = $(this).next('ul');
+            if (mainmenu.hasClass('open')) { 
+              mainmenu.hide().removeClass('open');
+            }
+            else {
+              mainmenu.show().addClass('open');
+              if (settings.format === "dropdown") {
+                mainmenu.find('ul').show();
+              }
+            }
+          });
+
+          multiTg = function() {
+            cssmenu.find(".has-sub").prepend('<span class="submenu-button"></span>');
+            cssmenu.find('.submenu-button').on('click', function() {
+              $(this).toggleClass('submenu-opened');
+              if ($(this).siblings('ul').hasClass('open')) {
+                $(this).siblings('ul').removeClass('open').hide();
+              }
+              else {
+                $(this).siblings('ul').addClass('open').show();
+              }
+            });
+          };
+
+          if (settings.format === 'multitoggle') multiTg();
+          else cssmenu.addClass('dropdown');
+        }
+
+        else if (settings.format === 'select')
+        {
+          cssmenu.append('<select style="width: 100%"/>').addClass('select-list');
+          var selectList = cssmenu.find('select');
+          selectList.append('<option>' + settings.title + '</option>', {
+                                                         "selected": "selected",
+                                                         "value": ""});
+          cssmenu.find('a').each(function() {
+            var element = $(this), indentation = "";
+            for (i = 1; i < element.parents('ul').length; i++)
+            {
+              indentation += '-';
+            }
+            selectList.append('<option value="' + $(this).attr('href') + '">' + indentation + element.text() + '</option');
+          });
+          selectList.on('change', function() {
+            window.location = $(this).find("option:selected").val();
+          });
+        }
+
+        if (settings.sticky === true) cssmenu.css('position', 'fixed');
+
+        resizeFix = function() {
+          if ($(window).width() > settings.breakpoint) {
+            cssmenu.find('ul').show();
+            cssmenu.removeClass('small-screen');
+            if (settings.format === 'select') {
+              cssmenu.find('select').hide();
+            }
+            else {
+              cssmenu.find("#menu-button").removeClass("menu-opened");
+            }
+          }
+
+          if ($(window).width() <= settings.breakpoint && !cssmenu.hasClass("small-screen")) {
+            cssmenu.find('ul').hide().removeClass('open');
+            cssmenu.addClass('small-screen');
+            if (settings.format === 'select') {
+              cssmenu.find('select').show();
+            }
+          }
+        };
+        resizeFix();
+        return $(window).on('resize', resizeFix);
+
+      });
+  };
+})(jQuery);
+
+(function($){
+$(document).ready(function(){
+
+$(document).ready(function() {
+  $("#cssmenu").menumaker({
+    title: "ВЫБРАТЬ КОМНАТУ",
+    format: "dropdown"
+  });
+
+  $("#cssmenu a").each(function() {
+    var linkTitle = $(this).text();
+    $(this).attr('data-title', linkTitle);
+  });
+});
+});
+
+})(jQuery);
+
+
